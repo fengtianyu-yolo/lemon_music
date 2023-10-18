158a1549941b61a786bcc28d67d1f701d1844d74
fengtianyu <fengtianyu@zouyebang.com>
2023-10-18 22:17:37 +0800

    project init

---
+++ pc/LemonMusic/LemonMusic/LemonMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonMusicApp.swift
+//  LemonMusic
+//
+//  Created by 冯天宇 on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct LemonMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
