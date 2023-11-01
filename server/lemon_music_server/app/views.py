11ea7ccab2fd4308c7144134afbd2ab3bc15d5a3
fengtianyu <444069440@qq.com>
2023-11-01 22:53:47 +0800

    feat(server): 创建视图函数

--- server/lemon_music_server/app/views.py
+++ server/lemon_music_server/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import render
 
 # Create your views here.
+
+class Login(View):
+    
+    TEMPLATE = 'templates/login.html'
+
+    def get(self, request):
+        return render(request, template_name=self.TEMPLATE)
