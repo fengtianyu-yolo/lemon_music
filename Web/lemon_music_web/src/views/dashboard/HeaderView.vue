d16300695bd7ce143eb6d59cbf2d0ec9da212b76
fengtianyu <444069440@qq.com>
2023-11-28 08:33:52 +0800

    feat(web): 创建所需要的页面

---
+++ Web/lemon_music_web/src/views/dashboard/HeaderView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header-container">
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="logout-btn">
+            Logout
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+
+const title = ref('Dashboard')
+
+</script>
+
+<style scoped>
+.header-container {
+    background-color: #FFFFFF;
+    padding-left: 60px;
+    padding-right: 60px;
+
+    display: flex;
+    justify-content: space-between;
+}
+
+.title {
+    
+}
+
+.logout-btn {
+    background-color: var(--global-highlight-color);
+}
+
+</style>
