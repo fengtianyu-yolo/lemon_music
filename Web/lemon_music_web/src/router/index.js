fb23ae217d0dba78208350106bcbca207c964e36
fengtianyu <444069440@qq.com>
2023-11-28 08:36:08 +0800

    feat(web): 初始化Dashboard页面和MusicLib页面并注册到路由

--- Web/lemon_music_web/src/router/index.js
+++ Web/lemon_music_web/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/dashboard/HomeView.vue'
+import DashboardView from '../views/dashboard/DashboardView.vue'
+import MusicLibView from '../views/dashboard/MusicLibView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +8,12 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: HomeView
+      component: DashboardView
     },
     {
       path: '/music_lib',
       name: 'home',
-      component: HomeView
+      component: MusicLibView
     }
   ]
 })
---
+++ Web/lemon_music_web/src/views/dashboard/MusicLibView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        MusicLib
+    </div>
+</template>
