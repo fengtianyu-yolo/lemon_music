9f643f48ea5bb98f8838170bd1498e0fc581fe80
fengtianyu <444069440@qq.com>
2023-12-04 08:04:58 +0800

    feat(Web): 创建存储用户登录信息的store

---
+++ Web/lemon_music_web/src/stores/userStore.ts
@@ -0,0 +1,42 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+
+export class User {
+    username = ''
+    userId = ''
+
+    constructor(userId, username) {        
+        this.username = username;
+        this.userId = userId;
+    }
+}
+
+export const useUserStore = defineStore('user', () => {
+    
+    var localUser = new User('-1', '')
+
+    var localUserString = sessionStorage.getItem('user')    
+    if (localUserString == null) {
+        localUserString = localStorage.getItem('user');
+    }
+    if (localUserString != null) {
+        var localUserObj = JSON.parse(localUserString)
+        localUser = new User(localUserObj['userId'], localUserObj['username']);
+    } 
+    
+    const user = ref(localUser)    
+
+    function refreshUser(user) {
+        let uservalue = JSON.stringify(user)
+        sessionStorage.setItem('user', uservalue);
+        localStorage.setItem('user', uservalue);
+    }
+
+    function cleanUser() {
+        sessionStorage.removeItem('user')
+        localStorage.removeItem('user')
+    }
+
+    return { user, refreshUser, cleanUser }
+})
