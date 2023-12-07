a28d244f73c6ad5dc65a8de71f20e7182ed88f87
fengtianyu <444069440@qq.com>
2023-12-07 16:34:59 +0800

    feat(Web): 引入Element-UI,构建曲库页面

--- Web/lemon_music_web/src/views/dashboard/MusicLibView.vue
+++ Web/lemon_music_web/src/views/dashboard/MusicLibView.vue
@@ -1,5 +1,119 @@
 <template>
-    <div>
-        MusicLib
+    <div class="music-lib-container">
+        <div class="filter-container">
+            <!-- <select>
+                <option value="none">Add filter</option>
+                <option value="name">Name</option>
+                <option value="singer">Singer</option>
+            </select> -->
+            <el-select v-model="filter" class="song-filter-select m-2" style="height: 46px;" placeholder="Add filter" size="large">
+                <el-option v-for="option in filterOptions" :key="option.value" :label="option.label" :value="option.value"></el-option>
+            </el-select>
+
+            <el-input v-model="searchValue" placeholder="Search for a song by name or singer" @change="search" @input="search"/>
+        </div>
+
+        <el-table :data="listData" stripe :row-class-name="rowClassName">
+            <el-table-column prop="name" label="Name" width="180" />
+            <el-table-column prop="singer" label="Singer" width="180" />
+            <el-table-column prop="format" label="格式" width="180" />
+            <el-table-column prop="duration" label="时长" width="180" />
+        </el-table>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const filter = ref('')
+
+const searchValue = ref('')
+
+const filterOptions = ref([
+    {
+        value: 'name',
+        label: 'Name'
+    },
+    {
+        value: 'singer',
+        label: 'Singer'
+    }
+])
+
+const listData = ref([
+    {
+        name: '孤勇者',
+        singer: '陈奕迅',
+        format: 'flac',
+        duration: '03:21'
+    },
+    {
+        name: '孤勇者',
+        singer: '陈奕迅',
+        format: 'flac',
+        duration: '03:21'
+    },
+    {
+        name: '孤勇者',
+        singer: '陈奕迅',
+        format: 'flac',
+        duration: '03:21'
+    },
+    {
+        name: '孤勇者',
+        singer: '陈奕迅',
+        format: 'flac',
+        duration: '03:21'
+    },
+    {
+        name: '孤勇者',
+        singer: '陈奕迅',
+        format: 'flac',
+        duration: '03:21'
+    },
+])
+
+function search() {
+    console.log(searchValue.value)
+}
+
+
+function rowClassName() {
+    return 'row'
+}
+
+</script>
+
+<style scoped>
+
+.music-lib-container {
+    background-color: #FFFFFF;
+}
+
+.filter-container {
+    margin-left: 40px;
+    margin-right: 100px;
+
+    height: 48px;
+    background-color: clear;
+
+    display: flex;
+}
+
+.song-filter-select {
+    width: 132px;
+    height: 48px;
+}
+
+.el-table {
+    margin-left: 40px;
+    margin-right: 100px;
+    margin-top: 10px;
+
+    width: 91%;
+}
+
+.row {
+    height: 48px;
+}
+</style>
