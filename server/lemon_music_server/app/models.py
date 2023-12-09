c51689f227c88df1cdfa1595a73cb6d3916c619b
fengtianyu <444069440@qq.com>
2023-12-09 09:43:36 +0800

    feat(Server): 实现数据同步接口,获取音频元数据,文件md5值获取

--- server/lemon_music_server/app/models.py
+++ server/lemon_music_server/app/models.py
@@ -6,7 +6,7 @@ class SingerModel(models.Model):
 	歌手表
 	"""
 	# 歌手ID
-	singer_id = models.IntegerField(primary_key=True)
+	singer_id = models.IntegerField(primary_key=True, auto_created=True)
 	# 歌手名字 
 	singer_name = models.CharField(max_length=255, null=False, default='')
 
--- server/lemon_music_server/app/views.py
+++ server/lemon_music_server/app/views.py
@@ -1,7 +1,15 @@
+from typing import Any
 from django.shortcuts import render, HttpResponse
 from django.views.generic import View
 from django.shortcuts import render, redirect
+
 import json
+import os
+from tinytag import TinyTag
+import hashlib
+
+from .models import SongModel
+from .models import SingerModel
 
 # Create your views here.
 
@@ -32,7 +40,10 @@ class LoginAPI(View):
         }
         return HttpResponse(json.dumps(result))
     
+
 class RefreshList(View):
+
+    unsupport_files = []
 
     def get(self, request):
         result = {
@@ -43,7 +54,66 @@ class RefreshList(View):
         }        
         return HttpResponse(json.dumps(result)) 
     
+
     def refresh(self): 
+
         # 拿到目录下的所有文件 
         path = '/Volumes/Elements SE/音乐库'
-        pass
+        song_list = os.listdir(path)
+        
+        # 2 遍历所有的文件  
+        for song_name in song_list:    
+            file_path = path + '/' + song_name
+    
+            try:
+                tag = TinyTag.get(file_path)
+                print(f'歌曲={tag.title},歌手={tag.artist},时长={tag.duration} 文件名={song_name}') 
+
+                # 获取文件的MD5 
+                file_hash_value = self.checksum(file_path)
+                                
+                # 文件在数据库中是否已经存在；已存在则跳过，不存在则插入到数据库 
+                exist = SongModel.objects.filter(id=file_hash_value).exists()
+                
+                if not exist:
+                    # 歌手是否已经存在
+                    self.update_singer(tag)     
+                    # 更新歌曲信息
+                    self.update_song(tag)                                   
+                
+            except Exception as e:
+                print(f"Error reading audio info: {e} {song_name}")
+                self.unsupport_files.append(song_name)
+                # 记录未解析文件 
+        
+
+    def update_song(self, song_info: TinyTag):
+        """
+        更新歌曲到数据库
+        """
+        if song_info.title == None:
+            print(f"没有读取到数据文件= {song_name}")
+            self.unsupport_files.append(song_name)
+        else:
+            song_name = song_info.title
+            singer = song_info.artist 
+            duration = song_info.duration
+            
+
+    def update_singer(self, song_info: TinyTag):
+        singer = song_info.artist
+        SingerModel.objects.filter()
+        
+
+    def checksum(self, filename):
+        """
+        获取大文件的哈希值
+        """
+        file_hash = hashlib.md5()
+        with open(filename, 'rb') as f:
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
+        
+        print(file_hash.hexdigest())
+        return file_hash.digest()
+    
