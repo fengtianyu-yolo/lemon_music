ff1fef4067c330853a309193abad1fb3ecca248e
fengtianyu <444069440@qq.com>
2024-09-07 16:44:26 +0800

    feature(server): 已添加到数据库的文件设置绿色标签，解析失败的添加灰色标签；过滤掉隐藏文件

--- server/yolo_music/app/script.py
+++ server/yolo_music/app/script.py
@@ -1,5 +1,6 @@
 import os 
 import mutagen
+from xattr import xattr
 
 root_path = '/Users/fengtianyu/Downloads/sample' 
 
@@ -44,6 +45,69 @@ def retrive_song_info(file_path):
     print('时长 = ' + str(duration))
     
     
+# travel(root_path)
+    
+def get_file_label():
+    file_path = '/Users/fengtianyu/Downloads/IMG_7320.JPG'
+    attrs = xattr(file_path)
+    info = attrs['com.apple.FinderInfo']
+    print(info)
+    color = info[9] >> 1 & 7
+    print(color)
+
+def set_label(filename, color_name):
+    colors = ['none', 'gray', 'green', 'purple', 'blue', 'yellow', 'red', 'orange']
+    key = u'com.apple.FinderInfo'
+    attrs = xattr(filename)
+    current = attrs.copy().get(key, chr(0)*32)
+    value = chr(colors.index(color_name)*2)
+    changed = current[:9] + str.encode(value) + current[10:]
+    attrs.set(key, changed)
+
+get_file_label()
+
+from enum import Enum 
+
+class ColorLabel(Enum):
+    none = 'none'
+    gray = 'gray'
+    green = 'green'
+    purple = 'purple'
+    blue = 'blue'
+    yellow = 'yellow'
+    red = 'red'
+    orange = 'orange'
+
+class Utils(object):
+    
+    colors = ['none', 'gray', 'green', 'purple', 'blue', 'yellow', 'red', 'orange']
+
+    @staticmethod
+    def get_file_label(filename) -> ColorLabel:
+        attrs = xattr(filename)
+        info = attrs['com.apple.FinderInfo']
+        print(info)
+        color = info[9] >> 1 & 7
+        rawvalue = Utils.colors[color] 
+        return ColorLabel(rawvalue)
+
+    @staticmethod
+    def set_label(filename, color_name):        
+        key = u'com.apple.FinderInfo'
+        attrs = xattr(filename)
+        current = attrs.copy().get(key, chr(0)*32)
+        value = chr(Utils.colors.index(color_name)*2)
+        changed = current[:9] + str.encode(value) + current[10:]
+        attrs.set(key, changed)
 
 
-travel(root_path)
+
+result = Utils.get_file_label('/Users/fengtianyu/Downloads/IMG_7320.JPG')
+
+print(result)
+
+Utils.set_label('/Users/fengtianyu/Downloads/IMG_7320.JPG', 'green')
+
+# set_label('/Users/fengtianyu/Downloads/IMG_7320.JPG', 'green')
+    
+# finder_colors.set('/Users/fengtianyu/Downloads/IMG_7320.JPG', 'green')
--- server/yolo_music/app/views.py
+++ server/yolo_music/app/views.py
@@ -6,6 +6,8 @@ from enum import Enum
 from .models import SongModel, ArtistModel, Song2ArtistModel
 import os
 import mutagen
+from .Utils import Utils, ColorLabel
+
 # Create your views here.
 
 def test(request):
@@ -43,64 +45,87 @@ class RefreshList(View):
     def travel(self, path) -> list[str]:
         result = []
         sub_itmes = os.listdir(path)
-        for item in sub_itmes:
+        files = list(filter(lambda x: not x.startswith('.') , sub_itmes))
+        
+        for item in files:                        
             full_path = path + '/' + item
             name = self.retrive_song_info(full_path, item)
             result.append(name)
         
+        return result 
+        
+
     def retrive_song_info(self, file_path, file_name) -> str:
-        
-        song = mutagen.File(file_path)
 
-        # 去重检查 
-        if self.exist(file_name):
-            print('数据库中已经存在,' + file_name)
+        try:
+            song = mutagen.File(file_path)
+        except:
+            print('文件无法解析: ' + file_name)
             return ''
-
-        songModel = SongModel() 
-        artist = ArtistModel()
-        song_artist = Song2ArtistModel() 
-        file_type = song.mime[0] 
-        
-        if file_type == 'audio/flac':
-            song_name = song.tags['TITLE']
-            artist_name = song.tags['ARTIST']            
-            songModel.media_type = 1
-            songModel.song_name = song_name
-            songModel.media_type = MediaType.FLAC.value
-            songModel.sq_file_path = file_path 
-            songModel.sq_file_name = file_name
-            artist.artist_name = artist_name            
-            # print('名字 = ' + str(song_name))
-            # print('歌手 = ' + str(artist_name))
-            
-        elif file_type == 'audio/mp3':            
-            song_name = song.tags['TIT2']
-            artist_name = song.tags['TPE1']
-            songModel.song_name = song_name
-            songModel.media_type = MediaType.MP3.value 
-            songModel.hq_file_path = file_path 
-            songModel.hq_file_name = file_name
-            artist.artist_name = artist_name
-            # print('file is mp3')
-            # print('名字 = ' + str(song_name))
-            # print('歌手 = ' + str(artist_name))
-
         else:
-            print('处理失败: ' + file_name)
-            return ''
+            if song == None:
+                Utils.set_label(file_path, ColorLabel.gray.value)
+                print('处理失败: ' + file_name)
+                return
 
-        duration = song.info.length
-        songModel.duration = duration
-        
-        song_artist.song = songModel 
-        song_artist.artist = artist
+            # 去重检查 
+            if self.exist(file_name):
+                print('数据库中已经存在,' + file_name)
+                return ''
 
-        songModel.save() 
-        artist.save() 
-        song_artist.save() 
-        return file_name
-        # print('时长 = ' + str(duration))
+            songModel = SongModel() 
+            artist = ArtistModel()
+            song_artist = Song2ArtistModel() 
+            file_type = song.mime[0] 
+            
+            if file_type == 'audio/flac':
+                try:
+                    song_name = song.tags['TITLE']
+                    artist_name = song.tags['ARTIST']            
+                except:
+                    print('FLAC处理失败: ' + file_name)
+                    Utils.set_label(file_path, ColorLabel.gray.value)
+                    return ''
+                else:
+                    songModel.media_type = 1
+                    songModel.song_name = song_name
+                    songModel.media_type = MediaType.FLAC.value
+                    songModel.sq_file_path = file_path 
+                    songModel.sq_file_name = file_name
+                    artist.artist_name = artist_name            
+                
+            elif file_type == 'audio/mp3':            
+                try:
+                    song_name = song.tags['TIT2']
+                    artist_name = song.tags['TPE1']
+                except:
+                    print('MP3处理失败: ' + file_name)
+                    Utils.set_label(file_path, ColorLabel.gray.value)
+                    return ''
+                else:
+                    songModel.song_name = song_name
+                    songModel.media_type = MediaType.MP3.value 
+                    songModel.hq_file_path = file_path 
+                    songModel.hq_file_name = file_name
+                    artist.artist_name = artist_name
+
+            else:
+                print('处理失败: ' + file_name)
+                Utils.set_label(file_path, ColorLabel.gray.value)
+                return ''
+
+            duration = song.info.length
+            songModel.duration = duration
+            
+            song_artist.song = songModel 
+            song_artist.artist = artist
+
+            songModel.save() 
+            artist.save() 
+            song_artist.save() 
+            Utils.set_label(file_path, ColorLabel.green.value)
+            return file_name
+            # print('时长 = ' + str(duration))
 
     def exist(self, file_name) -> bool: 
         SongModel.objects.filter(sq_file_name=file_name)
